6f9e17dba70d62dc2377f3371ded9310dc17485c
Ben Galewsky <bengal1@illinois.edu>
2020-02-20 16:23:54 -0600

    Merge latest train script

--- scripts/heptrx_nnconv.py
+++ scripts/heptrx_nnconv.py
@@ -35,8 +35,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('using device %s'%device)
 
 import logging
-    
-def main(args):    
+
+def main(args):
 
     path = osp.join(os.environ['GNN_TRAINING_DATA_ROOT'], args.dataset)
     print(path)
@@ -46,7 +46,7 @@ def main(args):
     tv_num = math.ceil(fulllen*tv_frac)
     splits = np.cumsum([fulllen-tv_num,0,tv_num])
     print(fulllen, splits)
-    
+
     train_dataset = torch.utils.data.Subset(full_dataset,np.arange(start=0,stop=splits[0]))
     valid_dataset = torch.utils.data.Subset(full_dataset,np.arange(start=splits[1],stop=splits[2]))
     train_loader = DataLoader(train_dataset, batch_size=train_batch_size, pin_memory=True)
@@ -58,7 +58,7 @@ def main(args):
     d = full_dataset
     num_features = d.num_features
     num_classes = d[0].y.dim() if d[0].y.dim() == 1 else d[0].y.size(1)
-    
+
     if args.categorized:
         if not args.forcecats:
             num_classes = int(d[0].y.max().item()) + 1 if d[0].y.dim() == 1 else d[0].y.size(1)
@@ -66,35 +66,36 @@ def main(args):
             num_classes = args.cats
 
 
-    the_weights = np.array([1., 1., 1., 1.]) #[0.017, 1., 1., 10.]
-    trainer = GNNTrainer(category_weights = the_weights, 
-                         output_dir='/home/lagray/hgcal_ldrd/', device=device)
+    #the_weights = np.array([1., 1., 1., 1.]) #[0.017, 1., 1., 10.]
+    the_weights = np.array([1., 1.])
+    trainer = GNNTrainer(category_weights = the_weights,
+                         output_dir='/data/gnn_code/hgcal_ldrd/output', device=device)
 
     trainer.logger.setLevel(logging.DEBUG)
     strmH = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     strmH.setFormatter(formatter)
     trainer.logger.addHandler(strmH)
-        
+
     #example lr scheduling definition
     def lr_scaling(optimizer):
-        from torch.optim.lr_scheduler import ReduceLROnPlateau        
+        from torch.optim.lr_scheduler import ReduceLROnPlateau
         return ReduceLROnPlateau(optimizer, mode='min', verbose=True,
-                                 min_lr=5e-7, factor=0.2, 
+                                 min_lr=5e-7, factor=0.2,
                                  threshold=0.01, patience=5)
-        
-    
+
+
     trainer.build_model(name=args.model, loss_func=args.loss,
                         optimizer=args.optimizer, learning_rate=args.lr, lr_scaling=lr_scaling,
                         input_dim=num_features, hidden_dim=args.hidden_dim, n_iters=args.n_iters,
                         output_dim=num_classes)
-    
+
     trainer.print_model_summary()
-        
+
     train_summary = trainer.train(train_loader, n_epochs, valid_data_loader=valid_loader)
-    
+
     print(train_summary)
-    
+
 
 if __name__ == "__main__":
 
@@ -109,7 +110,6 @@ if __name__ == "__main__":
     parser.add_argument('--hidden_dim', default=64, type=int, help='Latent space size.')
     parser.add_argument('--n_iters', default=6, type=int, help='Number of times to iterate the graph.')
     parser.add_argument('--dataset', '-d', default='single_photon')
-    
+
     args = parser.parse_args()
     main(args)
-                                                
